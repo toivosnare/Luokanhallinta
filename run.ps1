93453de654c1ad51a76bb60f4eecf6d23e968ab6
Snåre <toivo.snare@gmail.com>
2019-03-07 14:52:30 +0200

    Revert "These changes will be reverted"
    This reverts commit bcc4d9c82ff48b8c67e0c2dab10aa6c2bd942bc1.

--- run.ps1
+++ run.ps1
@@ -1,9 +1,9 @@
 param([Switch]$admin)
 Add-Type -AssemblyName System.Windows.Forms
 
-# $classFilePath = "$PSScriptRoot\luokka.csv" # $PSScriptRoot is the folder where this script is located
-$addonSyncPath = "\\10.132.0.24\Addons"
-$vbs3Path = "C:\Program Files\Bohemia Interactive Simulations\VBS3 3.9.0.FDF EZYQC_FI"
-$steelBeastsPath = "C:\Program Files\eSim Games\SB Pro FI\Release"
+$classFilePath = "$PSScriptRoot\luokka.csv" # $PSScriptRoot is the folder where this script is located
+$addonSyncPath = "\\10.132.0.97\Addons"
+$defaultGateway = "10.132.0.1"
+$internetGateway = "10.132.0.3"
 
 . $PSScriptRoot\hallinta.ps1
